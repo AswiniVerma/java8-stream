a692e3308dcf8186d6db5cb641baf87ebe01d082
AswiniVerma <72538717+AswiniVerma@users.noreply.github.com>
2022-04-21 10:43:07 +0530

    hey this is example

---
+++ GitHubProjectExample/src/main/java/com/basic/code/HelloWorldExample.java
@@ -0,0 +1,10 @@
+package com.basic.code;
+
+public class HelloWorldExample {
+
+	public static void main(String[] args) {
+		System.out.println("hi i am from Ranchi");
+		System.out.println("I learn java");
+	}
+
+}
